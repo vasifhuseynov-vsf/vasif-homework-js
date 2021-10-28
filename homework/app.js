cc61ff8e85dadef9dcd7ff727ff49ff32fec64c8
vasif-student <vasifah@code.edu.az>
2021-10-28 16:11:52 +0400

    homework

---
+++ homework/app.js
@@ -0,0 +1,38 @@
+// *** RETURN FIRST AND LAST NAME FROM A FORM ***
+
+
+// function getFirstLastName() {
+
+//     const first = document.getElementById("fname").value;
+//     const second = document.getElementById("lname").value;
+
+//     console.log(`First name: ${first}, Last name: ${second}`);
+// }
+
+// getFirstLastName();
+
+
+// **********************************************************************
+
+
+// *** SET BACKGROUND COLOR OF A PARAGRAPH ***
+
+
+// const para = document.getElementById("p");
+
+// para.style.backgroundColor = "red";
+
+
+// **********************************************************************
+
+
+// *** CHANGE THE FONT SIZE OF THE <p> TO 50PX ***
+
+
+// const para = document.getElementById('myText');
+
+// para.style.fontSize = "50px";
+
+
+
+// **********************************************************************
